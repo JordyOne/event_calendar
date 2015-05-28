05478e5ab5a6c730bee1575fc87313a8ae80b60d
Jordan Dale <Jordan.T.Dale@gmail.com>
2015-05-28 14:16:52 -0600

    final work

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,9 @@
 class EventsController < ApplicationController
   def index
     @tracks = Track.all
-    if params[:date]
-      @date = Date.strptime(params[:date])
+    if params[:event]
+      event = Event.find(params[:event].first)
+      @date = event.start
     else
       @date = Event.all.sample.start
     end
