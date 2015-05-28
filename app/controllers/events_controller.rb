bb3ae5087d049d16d3cac088bffbb327cfe60aff
Jordan Dale <Jordan.T.Dale@gmail.com>
2015-05-28 14:48:30 -0600

    added strong params to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,18 @@
 class EventsController < ApplicationController
   def index
+    byebug
     @tracks = Track.all
     if params[:event]
-      event = Event.find(params[:event].first)
+      event = Event.find(event_params[:id])
       @date = event.start
     else
       @date = Event.all.sample.start
     end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:id)
   end
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,9 @@
 class Track < ActiveRecord::Base
   has_many :events, -> { order(:start) }
 
+  def self.actual_tracks
+    general_track = Track.find(1)
+    Track.all(conditions: ["id != ?", 1]).map { |track| track.merge(general_track) }
+  end
+
 end
